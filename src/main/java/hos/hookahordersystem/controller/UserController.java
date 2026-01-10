1f35ce14804ebc4ddbab48c9462e4020366807b5
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2026-01-10 18:30:05 +0400

    Initial develop version

--- src/main/java/hos/hookahordersystem/controller/UserController.java
+++ src/main/java/hos/hookahordersystem/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UserController {
     private final UserRepository userRepository;
 
